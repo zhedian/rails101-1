321989d36773c691bb10d68a3429e7abafa7e496
zhedian <cooldeer@126.com>
2017-03-20 16:24:36 +0800

    implement my posts with edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -19,10 +19,36 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group =Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+  end
+
+  def update
+    @group =Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @group =Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to account_posts_path
+
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
 end
